ae877228a6650a71a843229628a4759d2e23e789
Nicolas <feriolinicolas@gmail.com>
2024-09-24 18:39:25 -0300

    agregada comlumna nueva en la tabla genero

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # TPE2024-WEB2
 WEB2
-![web2](https://github.com/user-attachments/assets/56fb8da0-8783-4816-ae25-48876481f7e6)
+![alt text](image.png)
 
 
 Base de datos de Peliculas con sus autores y sus respectivos generos.
 
 Posee dos tablas:
   **PRIMER TABLA**: Peliculas (ID, Titulo, Autor, Año, ID_genero [clave foránea]).
-  **SEGUNDA TABLA**: Generos (ID, Nombre, Descripción).
+  **SEGUNDA TABLA**: Generos (ID, Nombre, Descripción, Clasificacion por edad).
   
 Las tablas tiene una relacion 1 a N (N=Peliculas), es decir, un género puede relacionarse a muchas peliculas (o ninguna), y una pelicula relacionarse a un solo género.
 
--- tpe2024.sql
+++ tpe2024.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-09-2024 a las 00:10:48
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2024 at 11:28 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,35 +18,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tpe2024`
+-- Database: `tpe2024`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `generos`
+-- Table structure for table `generos`
 --
 
 CREATE TABLE `generos` (
   `ID_Genero` int(11) NOT NULL,
   `Nombre` varchar(250) NOT NULL,
-  `Descripción` varchar(250) NOT NULL
+  `Descripción` varchar(250) NOT NULL,
+  `clasificacion_por_edad` varchar(255) DEFAULT 'Sin clasificar'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `generos`
+-- Dumping data for table `generos`
 --
 
-INSERT INTO `generos` (`ID_Genero`, `Nombre`, `Descripción`) VALUES
-(1, 'Acción', 'Acción. Generalmente son películas que aportan un toque de adrenalina. Incluyen acrobacias físicas, persecuciones rescates y batallas, lo que las caracteriza principalmente.'),
-(3, 'Drama', 'Drama is the specific mode of fiction represented in performance: a play, opera, mime, ballet, etc., performed in a theatre, or on radio or television. '),
-(4, 'Terror', 'género que pretende o tiene la capacidad de asustar, causar miedo o aterrorizar sus lectores o espectadores e inducir sentimientos de horror y terror.'),
-(5, 'Ciencia ficcion', 'Science fiction is a genre of speculative fiction, which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It is related to');
+INSERT INTO `generos` (`ID_Genero`, `Nombre`, `Descripción`, `clasificacion_por_edad`) VALUES
+(1, 'Acción', 'Acción. Generalmente son películas que aportan un toque de adrenalina. Incluyen acrobacias físicas, persecuciones rescates y batallas, lo que las caracteriza principalmente.', 'Sin clasificar'),
+(3, 'Drama', 'Drama is the specific mode of fiction represented in performance: a play, opera, mime, ballet, etc., performed in a theatre, or on radio or television. ', 'Sin clasificar'),
+(4, 'Terror', 'género que pretende o tiene la capacidad de asustar, causar miedo o aterrorizar sus lectores o espectadores e inducir sentimientos de horror y terror.', 'Sin clasificar'),
+(5, 'Ciencia ficcion', 'Science fiction is a genre of speculative fiction, which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It is related to', 'Sin clasificar');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `peliculas`
+-- Table structure for table `peliculas`
 --
 
 CREATE TABLE `peliculas` (
@@ -58,7 +59,7 @@ CREATE TABLE `peliculas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `peliculas`
+-- Dumping data for table `peliculas`
 --
 
 INSERT INTO `peliculas` (`ID_pelicula`, `Titulo`, `Director`, `Anio`, `ID_Genero`) VALUES
@@ -68,44 +69,44 @@ INSERT INTO `peliculas` (`ID_pelicula`, `Titulo`, `Director`, `Anio`, `ID_Genero
 (4, 'Interestellar', 'Christopher Nolan', 2014, 5);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `generos`
+-- Indexes for table `generos`
 --
 ALTER TABLE `generos`
   ADD PRIMARY KEY (`ID_Genero`);
 
 --
--- Indices de la tabla `peliculas`
+-- Indexes for table `peliculas`
 --
 ALTER TABLE `peliculas`
   ADD PRIMARY KEY (`ID_pelicula`),
   ADD KEY `ID_Generofk` (`ID_Genero`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `generos`
+-- AUTO_INCREMENT for table `generos`
 --
 ALTER TABLE `generos`
   MODIFY `ID_Genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT de la tabla `peliculas`
+-- AUTO_INCREMENT for table `peliculas`
 --
 ALTER TABLE `peliculas`
   MODIFY `ID_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `peliculas`
+-- Constraints for table `peliculas`
 --
 ALTER TABLE `peliculas`
   ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`ID_Genero`) REFERENCES `generos` (`ID_Genero`);
