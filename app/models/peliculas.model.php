7bd0ffde2aa6eb2051100e06916462c62bcf33a6
Nicolas <feriolinicolas@gmail.com>
2024-10-05 20:55:16 -0300

    agregadas funciones crud al modelo de peliculas

--- app/models/peliculas.model.php
+++ app/models/peliculas.model.php
@@ -6,4 +6,34 @@ class PeliculasModel {
        $this->db = new PDO('mysql:host=localhost;dbname=db_peliculas;charset=utf8', 'root', '');
     }
 
+    public function getPeliculas() {
+        $query = $this->db->prepare('SELECT * FROM peliculas');
+        $query->execute();
+        $peliculas = $query->fetchAll(PDO::FETCH_OBJ); 
+    
+        return $peliculas;
+    }
+
+    public function getPelicula($id) {
+        $query = $this->db->prepare('SELECT * FROM peliculas WHERE id=?');
+        $query->execute([$id]);
+        $pelicula = $query->fetch(PDO::FETCH_OBJ); 
+    
+        return $pelicula;
+    }
+
+    public function insertarPelicula($titulo, $descripcion, $director, $id_genero) {
+        $query = $this->db->prepare('INSERT INTO peliculas(titulo, descripcion, director, id_genero) VALUES(?,?,?,?)');
+        $query->execute([$titulo, $descripcion, $director, $id_genero]);
+    }
+
+    public function borrarPelicula($id) {
+        $query = $this->db->prepare('DELETE FROM peliculas WHERE id=?');
+        $query->execute([$id]);
+    }
+
+    public function editarPelicula($id, $titulo, $descripcion, $director, $id_genero) {
+        $query = $this->db->prepare('UPDATE peliculas SET titulo=?, descripcion=?, director=?, id_genero=? WHERE id=?');
+        $query->execute([$titulo, $descripcion, $director, $id_genero, $id]);
+    }
 }
