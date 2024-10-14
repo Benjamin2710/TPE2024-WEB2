2edb7872390a753f3c404fc8d05c5c5641646b71
Nicolas <feriolinicolas@gmail.com>
2024-10-13 21:28:45 -0300

    agregado .htaccess

---
+++ app/models/generos.model.php
@@ -0,0 +1,16 @@
+<?php
+class GenerosModel {
+    private $db;
+
+    public function __construct() {
+       $this->db = new PDO('mysql:host=localhost;dbname=db_peliculas;charset=utf8', 'root', '');
+    }
+
+    public function getGeneros() {
+        $query = $this->db->prepare('SELECT * FROM generos');
+        $query->execute();
+        $generos = $query->fetchAll(PDO::FETCH_OBJ); 
+    
+        return $generos;
+    }
+}
