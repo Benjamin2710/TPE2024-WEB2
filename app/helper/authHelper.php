072bdada02b8b598ad5f673adfca3d8672c21752
Nicolas <feriolinicolas@gmail.com>
2024-10-17 02:44:04 -0300

    self para llamarse a si mismo en authhelper

--- app/helper/authHelper.php
+++ app/helper/authHelper.php
@@ -9,18 +9,18 @@ class AuthHelper {
     }
 
     public static function login($usuario) {
-        AuthHelper::init();
+        self::init();
         $_SESSION['id_usuario'] = $usuario->id_usuario;
         $_SESSION['usuario'] = $usuario->usuario; 
     }
 
     public static function logout() {
-        AuthHelper::init();
+        self::init();
         session_destroy();
     }
 
     public static function verify() {
-        AuthHelper::init();
+        self::init();
         if (!isset($_SESSION['id_usuario'])) {
             header('Location: ' . BASE_URL . 'iniciosesion');
             die();
