b3fd74acf81fcbe195b3923700408624ee8ea32d
Nicolas <feriolinicolas@gmail.com>
2024-10-17 03:18:48 -0300

    refactor: cambio de user a username en login y signup y usermodel, se agrega un nuevo campo en signup llamado "role"

--- app/models/user.model.php
+++ app/models/user.model.php
@@ -7,9 +7,9 @@ class UserModel {
        $this->db = new PDO('mysql:host=localhost;dbname=db_peliculas;charset=utf8', 'root', '');
     }
  
-    public function getUserByEmail($email) {    
-        $query = $this->db->prepare("SELECT * FROM usuario WHERE email = ?");
-        $query->execute([$email]);
+    public function getUserByUsername($username) {    
+        $query = $this->db->prepare("SELECT * FROM usuario WHERE username = ?");
+        $query->execute([$username]);
     
         $user = $query->fetch(PDO::FETCH_OBJ);
     
