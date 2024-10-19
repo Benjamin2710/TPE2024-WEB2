131a4f5ce4d4c9fa8499bb51d00d60172ab3f7ff
Nicolas <feriolinicolas@gmail.com>
2024-10-19 05:19:17 -0300

    borrar dentro de la pagina item

--- frontend/js/item.js
+++ frontend/js/item.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    let scrollear = true;
     document.getElementById('edit-button').addEventListener('click', function() {
         document.getElementById('formPelicula').classList.toggle('hidden');
+
+        if (scrollear) {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+            scrollear = false;
+        }else{
+            scrollear = true;
+        }
+        
     });
 });
